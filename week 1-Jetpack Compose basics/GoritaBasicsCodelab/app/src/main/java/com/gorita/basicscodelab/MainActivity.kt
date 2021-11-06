d0d42fa2a3a3dfc221848793440d7603dc5b07a8
gerard <gerard@kcd.co.kr>
2021-11-07 02:52:39 +0900

    Create performant lazy list

--- week 1-Jetpack Compose basics/GoritaBasicsCodelab/app/src/main/java/com/gorita/basicscodelab/MainActivity.kt
+++ week 1-Jetpack Compose basics/GoritaBasicsCodelab/app/src/main/java/com/gorita/basicscodelab/MainActivity.kt
@@ -3,16 +3,12 @@ package com.gorita.basicscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -30,15 +26,36 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun OnboardingScreen(onContinueClicked: () -> Unit) {
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Welcome to the Basics Codelab!")
+            Button(
+                modifier = Modifier.padding(vertical = 24.dp),
+                onClick = onContinueClicked
+            ) {
+                Text(text = "Continue")
+            }
+        }
+    }
+}
+
+@Composable
 fun Greeting(name: String) {
-    var expanded = remember { mutableStateOf(false) }
+    var expanded: MutableState<Boolean> = remember { mutableStateOf(false) }
     val extraPadding = if (expanded.value) 48.dp else 0.dp
     Surface(
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
         Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier.weight(1f)
+            Column(modifier = Modifier
+                .weight(1f)
                 .padding(bottom = extraPadding)
             ) {
                 Text(text = "Hello, ")
@@ -54,11 +71,29 @@ fun Greeting(name: String) {
 }
 
 @Composable
-private fun MyApp(names: List<String> = listOf("World", "Compose")) {
-    Column {
-        for (name in names) {
+private fun MyApp() {
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+    if (shouldShowOnboarding) {
+        OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+    } else {
+        Greetings()
+    }
+}
+
+@Composable
+private fun Greetings(names: List<String> = List(1000) { "Sit" }) {
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+        items(items = names) { name ->
             Greeting(name = name)
         }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    GoritaBasicsCodelabTheme {
+        OnboardingScreen(onContinueClicked = {/* explicitly empty */})
     }
 }
 
@@ -66,6 +101,6 @@ private fun MyApp(names: List<String> = listOf("World", "Compose")) {
 @Composable
 fun DefaultPreview() {
     GoritaBasicsCodelabTheme {
-        MyApp()
+        Greetings()
     }
 }
